bf06cb5bf118b2122bbd90dc89d41d1ec7f89488
Kushwahaganesh <114466594+Kushwahaganesh@users.noreply.github.com>
2022-11-07 23:07:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 
 st.title("Cryptocurrency Daily Prices | ₿")
 st.header("Main Dashboard")
-st.subheader("you can add more crypto in code </
+st.subheader("you can add more crypto in code </) 
 
 Bitcoin = 'BTC-USD'
 Ethereum = 'ETH-USD'
