34e2d34076128bd1c0034e96725034974b3fadee
Kushwahaganesh <114466594+Kushwahaganesh@users.noreply.github.com>
2022-11-09 21:48:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import streamlit as st
 from PIL import Image
-from urllib.request import urlopen
+
 
 st.title("Cryptocurrency Daily Prices | ₿")
 st.header("Main Dashboard")
