0e0ffe9d3ff7c98d38fb98ec7ab952ce8a6f8559
Kushwahaganesh <114466594+Kushwahaganesh@users.noreply.github.com>
2022-11-09 22:19:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,49 +24,37 @@ ETHHis = ETH_Data.history(period="max")
 XRPHis = XRP_Data.history(period="max")
 BCHHis = BCH_Data.history(period="max")
 
-BTC = yf.download(Bitcoin, start="2021-11-19", end="2021-11-19")
-ETH = yf.download(Ethereum, start="2021-11-19", end="2021-11-19")
-XRP = yf.download(Ripple, start="2021-11-19", end="2021-11-19")
-BCH = yf.download(BitcoinCash, start="2021-11-19", end="2021-11-19")
+BTC = yf.download(Bitcoin, start="2021-11-19", end="2021-11-20")
+ETH = yf.download(Ethereum, start="2021-11-19", end="2021-11-20")
+XRP = yf.download(Ripple, start="2021-11-19", end="2021-11-20")
+BCH = yf.download(BitcoinCash, start="2021-11-19", end="2021-11-20")
 
 
 st.write("BITCOIN ($)")
 imageBTC = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'))
-#Display image
 st.image(imageBTC)
-#Display dataframe
 st.table(BTC)
-#Display a chart
 st.bar_chart(BTCHis.Close)
 
 
 st.write("ETHERUM ($)")
 imageETH = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'))
-#Display image
 st.image(imageETH)
-#Display dataframe
 st.table(ETH)
-#Display a chart
 st.bar_chart(ETHHis.Close)
 
 
 st.write("RIPPLE ($)")
 imageXRP = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/52.png'))
-#Display image
 st.image(imageXRP)
-#Display dataframe
 st.table(XRP)
-#Display a chart
 st.bar_chart(XRPHis.Close)
 
 
 st.write("BITCOIN CASH ($)")
 imageBCH = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1831.png'))
-#Display image
 st.image(imageBCH)
-#Display dataframe
 st.table(BCH)
-#Display a chart
 st.bar_chart(BCHHis.Close)
 
 
